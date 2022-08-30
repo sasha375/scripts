1791db9a5d2d4ca6481a0c977397d9f7924e0655
stacksofblue5 <s.kk.kravchenko@yandex.ru>
2022-08-30 12:05:37 +0300

    using get

--- funcs.py
+++ funcs.py
@@ -2,13 +2,7 @@ from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
                                 Schoolkid, Subject)
 
 def find_kid(name):
-    kids = Schoolkid.objects.filter(full_name__contains=name)
-    if len(kids) == 1:
-        return kids[0]
-    elif len(kids) > 1:
-        raise Exception("Multiple kids found")
-    else:
-        raise Exception("No kids found")
+    return Schoolkid.objects.get(full_name__contains=name)
 
 def all_fives(kid):
      marks = Mark.objects.filter(schoolkid=kid)
@@ -20,5 +14,5 @@ def remove_chastisements(kid):
 
 def create_commendation(kid, subject_name, text="Хвалю!"):
     maths = Subject.objects.filter(title=subject_name, year_of_study=kid.year_of_study)
-    lesson = Lesson.objects.filter(subject__in=maths, group_letter=kid.group_letter)[0]
+    lesson = Lesson.objects.get(subject__in=maths, group_letter=kid.group_letter)
     Commendation.objects.create(text=text, created=lesson.date, schoolkid=kid, subject=lesson.subject, teacher=lesson.teacher)
